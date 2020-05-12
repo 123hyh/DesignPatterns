70f6f15a8fccb3607f52a78d9dea9723934524b9
herding-wolf <herding.wolf.lc@gmail.com>
2020-05-12 17:27:23 +0800

    no message

---
+++ index.ts
@@ -0,0 +1,16 @@
+/* import { ReleaseSubscription } from "./releaseSubscription";
+
+let data = { name: "hyh" };
+var controlCenter = new (ReleaseSubscription())(data);
+const handler1 = () => {
+  console.log(1);
+};
+controlCenter.subscribe(handler1);
+
+controlCenter.subscribe(() => {
+  console.log(2);
+});
+
+controlCenter.unSubscribe(handler1);
+debugger;
+ */
---
+++ releaseSubscription.ts
@@ -0,0 +1,99 @@
+type Handler = (...arg: any[]) => void;
+
+
+function createProxy<T extends any>(this: T, data: any) {
+  return new Proxy(data, {
+    set: (target, key, value) => {
+      // 数组在设置值时会设置多一次下标
+      if (Array.isArray(target) && key === "length" && !!value) return true;
+
+      const prevData = { [key]: target[key] };
+      target[key] = value;
+      if (typeof value === "object") {
+        target[key] = createProxy.call(this, value);
+      }
+      /**
+       * 异步调用
+       */
+      Promise.resolve().then(() => {
+        this.publish(JSON.parse(JSON.stringify(target)), prevData);
+      });
+      return true;
+    },
+  });
+}
+
+/**
+ * 发布订阅模式
+ */
+function ReleaseSubscription() {
+  /**
+   * 订阅的中心(存放事件)
+   */
+  const events: Array<(...args: any[]) => void> = [];
+  return class ReleaseSubscription {
+    protected observer: any;
+    /**
+     * 创建发布订阅中心
+     */
+    static Create(data: any) {
+      const instance = new ReleaseSubscription(data);
+      return [instance.observer, instance];
+    }
+
+    constructor(data: any) {
+      this.observer = createProxy.call(this, data);
+    }
+
+    /**
+     * 订阅
+     * @param {Function} handler 需要订阅的回调
+     */
+    subscribe(handler: Handler) {
+      events.push(handler);
+      return this;
+    }
+
+    /**
+     * 取消订阅
+     * @param { Function }  handler 取消订阅的回调
+     */
+    unSubscribe(handler: Handler) {
+      let i = 0;
+      while (i < events.length) {
+        const fn = events[i];
+        if (fn === handler) {
+          events.splice(i, 1);
+          break;
+        }
+        i++;
+      }
+    }
+
+    /**
+     * 取消所有订阅
+     */
+    unAllSubscribe() {
+      events.length = 0;
+    }
+
+    /**
+     * 发布消息
+     */
+    private publish(data?: any, prevData?: any) {
+      for (let handler of events) {
+        handler({ current: data, prev: prevData });
+      }
+    }
+  };
+}
+
+const [data, controlCenter] = ReleaseSubscription().Create({
+  name: "hyh",
+});
+
+const handler1 = (data: any) => {
+  console.log(data);
+};
+
+controlCenter.subscribe(handler1);
